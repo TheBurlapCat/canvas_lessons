5e53a4352136e371d139bf191b33799e44bd6a3e
Jenny A Hambleton <theburlapcat@gmail.com>
2015-09-07 22:03:39 -0600

    first animation

--- circles.js
+++ circles.js
@@ -2,9 +2,22 @@ var canvasElement = document.getElementById('circleCanvas');
 var renderingContext = canvasElement.getContext('2d');
 console.dir(renderingContext);
 
-renderingContext.strokeStyle = '#ffffff';
-renderingContext.beginPath();
-renderingContext.moveTo(100, 150);
-renderingContext.lineTo(450, 50);
-renderingContext.stroke();
+var position = {
+	x: 0,
+	y: 200
+};
 
+var render = function() {
+
+	window.requestAnimationFrame( render );
+
+	renderingContext.clearRect(0,0,800,400);
+	position.x += 10;
+	renderingContext.strokeStyle = '#ffffff';
+	renderingContext.beginPath();
+	renderingContext.moveTo(100, 150);
+	renderingContext.lineTo(position.x, position.y);
+	renderingContext.stroke();
+};
+
+render();
